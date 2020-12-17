9cff5f10daa5510ccb64e26298721f31055550c5
Prakharjainprince <prakharjain995@gmail.com>
2020-12-17 22:54:44 +0530

    testcase problem

--- src/component/BookContainer.tsx
+++ src/component/BookContainer.tsx
@@ -6,11 +6,11 @@ function BookContainer(props) {
     const [number, setNumber] = useState(1)
     return (
         <div>
-            <h1 id='title'>Book Component</h1>
+            <h1 id='titleBook'>Book Component</h1>
             <h2 id='noOfBook'>Number of Books : {props.numberOfBooks}</h2>
             <input type="text" value={number} onChange={e=>setNumber(e.target.value)} />
             <button onClick={() => props.buyBook(number)}>Buy {number} Book</button>
-        </div>
+        </div> 
     )
 }
 
--- tests/Component/BookContainer.test.tsx
+++ tests/Component/BookContainer.test.tsx
@@ -1,25 +1,48 @@
-import * as React from 'react';
-// import {mount} from 'enzyme';
-// import BookContainer from '../../src/component/BookContainer';
-// import {BrowserRouter as Router} from 'react-router-dom';
+import React from 'react';
+import { mount, shallow } from '../setup/test-setup';
+import BookContainer from '../../src/component/BookContainer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-describe("Addition of number", () => {
-    test('Two plus 2 is four', () => {
-        expect(2+2).toBe(4);
-    });
-});
 
-// describe('Book render', () => {
-//     it('Book title render', () => {
-//         const obj = mount(
-//             <Router><BookContainer /></Router>
-//         );
-//         expect(obj.find('#title').text()).toBe('Book Component');
-//     });
-//     it('NUmber of books render', () => {
-//         const obj = mount(
-//             <Router><BookContainer /></Router>
-//         );
-//         expect(obj.find('#noOfBook').text()).toBe('Number of Books :');
+// describe("Addition of number", () => {
+//     test('Two plus 2 is four', () => {
+//         expect(2+2).toBe(4);
 //     });
 // });
+
+describe('Render UI', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(
+            <BookContainer />
+        );
+    })
+
+    describe('Book render', () => {
+        it('Book title h1 render', () => {
+            expect(wrapper.find('#titleBook').text()).toBe('Book Component');
+        });
+    //     // it('NUmber of books render', () => {
+    //     //     expect(wrapper.find('#noOfBook').text()).toBe('Number of Books :');
+    //     // });
+    // });
+
+    // describe('Counter component', () => {
+    //     it('starts with no of book 1', () => {
+    //         const text = wrapper.find('h1.titleBook').text();
+    //         expect(text).toBe('Book Component')
+    //     })
+
+        // it('starts with no of book 1', () => {
+        //     const text = wrapper.find('h1.noOfBook').text()
+        //     expect(text).toEqual('Number of Books :')
+        // })
+
+        // it('can increment the count when the button is clicked', () => {
+        //     const incrementBtn = wrapper.find('button')
+        //     incrementBtn.simulate('click')
+        //     const text = wrapper.find('h2').text()
+        //     expect(text).toEqual('Number of Books : 1')
+        // })
+    })
+});
